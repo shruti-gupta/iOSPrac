9989acfeb7f7e91588246b41a7e8d009f3e72ada
shruti gupta <shrutigupta@shrutis-MacBook-Pro.local>
2014-05-30 23:23:34 +0530

    Initial Commit

---
+++ QuizApp/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  QuizApp
+//
+//  Created by shruti gupta on 30/05/14.
+//  Copyright (c) 2014 Shruti Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ QuizApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QuizApp
+//
+//  Created by shruti gupta on 30/05/14.
+//  Copyright (c) 2014 Shruti Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
